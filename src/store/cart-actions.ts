288e3196cb0d7ca7a48c4cf326e2b448d5e2ad38
samithperera94 <samiththilanka94@gmail.com>
2023-07-05 12:54:01 +0530

    replaced fetch with axios

--- src/store/cart-actions.ts
+++ src/store/cart-actions.ts
@@ -1,24 +1,34 @@
 import cart, { cartActions } from "./cart";
 import {cartState} from "./cart";
 import { ThunkDispatch,Dispatch,AnyAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const baseUrl = 'https://react-http-d7746-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json';
 
 
 
 export const sendCartData = (cart:cartState) => {
     return async () => {
         const sendRequest = async() => {
-            const response =  await fetch('https://react-http-d7746-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',{
-                method:'PUT',
-                body:JSON.stringify({
-                    items:cart.cartItems,
-                    totalQuantity:cart.totalQuantity,
-                    deliveryType:cart.cartItems
-                })
-            });
+            // const response =  await fetch('https://react-http-d7746-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',{
+            //     method:'PUT',
+            //     body:JSON.stringify({
+            //         items:cart.cartItems,
+            //         totalQuantity:cart.totalQuantity,
+            //         deliveryType:cart.cartItems
+            //     })
+            // });
 
-            if(!response.ok){
-                throw new Error("Sending Cart Data Failed!")
+            const sendData = {
+                items:cart.cartItems,
+                totalQuantity:cart.totalQuantity,
+                deliveryType:cart.cartItems
             }
+            axios.put(baseUrl,sendData).then((response)=>{
+                console.log(response.data);
+            }).catch((error)=>{
+                throw new Error("Sending Cart Data Failed!")
+            });
 
         }
 
@@ -36,28 +46,45 @@ export const getCartData = ()=> {
         cart: cartState;
     }, undefined, AnyAction> & Dispatch<AnyAction>)=>{
         const getData = async() => {
-            const response = await fetch('https://react-http-d7746-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json');
+            // const response = await fetch('https://react-http-d7746-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json');
     
-            if(!response.ok){
-                throw new Error("data retriening failed!");
-            }
+            // if(!response.ok){
+            //     throw new Error("data retriening failed!");
+            // }
             
-            const data = await response.json();
-            return data
+            // const data = await response.json();
+            // return data
+
+            axios.get(baseUrl).then(response => {
+                console.log("response.data !!!!",response.data);
+                const cartData = response.data;
+                const cart:cartState = {
+                    cartItems:cartData.items,
+                    totalQuantity:cartData.totalQuantity,
+                    deliveryType:cartData.deliveryType,
+                    isCartChanged:false
+                }
+
+                dispatch(cartActions.replaceCart({cart}))
+
+            }).catch((error)=>{
+                throw new Error("data retriving failed!");
+            })
         }
 
         try{
-            const cartData = await getData();
-            // console.log("cartData",cartData);
-            const cart:cartState = {
-                cartItems:cartData.items,
-                totalQuantity:cartData.totalQuantity,
-                deliveryType:cartData.deliveryType,
-                isCartChanged:false
-            }
-            dispatch(cartActions.replaceCart({cart}))
+            await getData();
+            // const cartData = await getData();
+            // console.log("cartData !!!!",cartData);
+            // const cart:cartState = {
+            //     cartItems:cartData.items,
+            //     totalQuantity:cartData.totalQuantity,
+            //     deliveryType:cartData.deliveryType,
+            //     isCartChanged:false
+            // }
+            // dispatch(cartActions.replaceCart({cart}))
         }catch(error){
-            
+            console.error(error)
         }
     }   
 
